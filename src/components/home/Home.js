2c0b6f56ee94e46447130cbb43b5832f854a9335
Erik Kučák <riko@ksp.sk>
2019-02-10 00:45:29 +0100

    Fixed little warnings

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import Navbar from "../navbar/Navbar"
-import Destination from "../destination/Destination"
-import "./Home.css"
+import Navbar from "../navbar/Navbar";
+import Destination from "../destination/Destination";
+import "./Home.css";
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <div className="home_part-1">
-            <Navbar/>
-            <div className="home-center">
-                <p id="its-time">It's time to pack and go! :{")"}</p>
-                <p id="travel-friends-home">Your travel friends are waiting for you!</p>
-                <button id="findthem">Find them!</button>
-            </div>
+          <Navbar />
+          <div className="home-center">
+            <p id="its-time">It is time to pack and go! :{")"}</p>
+            <p id="travel-friends-home">
+              Your travel friends are waiting for you!
+            </p>
+            <button id="findthem">Find them!</button>
+          </div>
         </div>
         <p>TODO</p>
       </div>
--- src/components/profileTrips/ProfileTrips.js
+++ src/components/profileTrips/ProfileTrips.js
@@ -2,45 +2,61 @@ import React, { Component } from "react";
 import "./ProfileTrips.css";
 
 class ProfileTrips extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form class="main-form" onSubmit={this.handleSubmit}>
-          <label className="destination">
-            Destination:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <label className="date">
-            from:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <label className="date">
-            to:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <label className="trip-info">
-            Additional info:
-            <textarea type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="main-form" onSubmit={this.handleSubmit}>
+        <label className="destination">
+          Destination:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className="date">
+          from:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className="date">
+          to:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className="trip-info">
+          Additional info:
+          <textarea
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default ProfileTrips;
