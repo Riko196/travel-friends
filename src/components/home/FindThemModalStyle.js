ce8979367ccaa6113b7ec762674432ff8c06f1c9
Ivan Agarsky <ivan.agarsky@innovatrics.com>
2019-03-30 19:11:51 +0100

    Find-travel-friends-modal

---
+++ src/components/home/FindThemModalStyle.js
@@ -0,0 +1,10 @@
+export const findThemModalStyle = {
+    content: {
+        overflow: 'auto',
+        position: 'absolute',
+        background: 'white',
+        padding: '1em',
+        borderRadius: '5px',
+        boxShadow: '0px 0px 15px 5px rgba(0,0,0,0.5)'
+    }
+};
