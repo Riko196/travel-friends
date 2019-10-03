99f9bf44865c088d0fe7e3761b2f3c33dff29cd6
Ivan Agarsky <ivan.agarsky@innovatrics.com>
2019-10-03 21:21:55 +0200

    back button

--- src/components/privacy/PrivacyPolicy.js
+++ src/components/privacy/PrivacyPolicy.js
@@ -6,13 +6,13 @@ class PrivacyPolicy extends Component {
     return (
       <div>
         <div className="background-image-privacy">
-          <NavLink to="/" className="navbar-link large-text-link">
-            Landing page
-          </NavLink>
           <img
             src={require("../../images/privacy_background.jpg")}
             alt={"Privacy Policy"}
           />
+          <NavLink to="/" className="back-link">
+            Back
+          </NavLink>
           <div className="privacy-container">
             <p className="privacy-p">PRIVACY POLICY</p>
           </div>
